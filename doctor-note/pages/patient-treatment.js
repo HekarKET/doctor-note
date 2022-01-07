d5c6fd0db2a4507b355166c29af00202a6dcee24
NIRAV <nirav.lengade7@gmail.com>
2022-01-07 14:00:40 +0530

    feat: patient/treatment route added and component created

--- doctor-note/pages/patient-treatment.js
+++ doctor-note/pages/patient-treatment.js
@@ -1,21 +1,59 @@
 import React from "react";
-import withAuth from '../util/auth';
+import withAuth from "../util/auth";
+import SelectCustom from "../util/Select";
+import { TextField } from "@material-ui/core";
 
- const addPatient = ()=> {
+
+const addPatient = () => {
   return (
     <>
-      <div className='container'>
-        <div className='dashboard-container'>
-
-
-        <div className='content'>
-          
-        </div>
-
-
+      <div className="container">
+        <div className="dashboard-container">
+        
+          <div className="content">
+            <button>Add Patient</button>
+            <TextField
+              placeholder='Patient Name *'
+              variant='filled'
+              required
+              color='primary'
+              fullWidth
+            />
+            <TextField
+              placeholder='Address'
+              variant='filled'
+              color='primary'
+              fullWidth
+            />
+            <TextField
+              placeholder='Age Range'
+              variant='filled'
+              color='primary'
+              fullWidth
+            />
+            <button>Add Treatment</button>
+            <TextField
+              placeholder='Patient ID'
+              variant='filled'
+              color='primary'
+              fullWidth
+            />
+            <TextField
+              placeholder='Treatment'
+              variant='filled'
+              color='primary'
+              fullWidth
+            />
+            <TextField
+              placeholder='Diagnosis'
+              variant='filled'
+              color='primary'
+              fullWidth
+            />
+          </div>
         </div>
       </div>
     </>
   );
-}
+};
 export default withAuth(addPatient);
